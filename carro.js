3370c1f5a71cae020b91c3b617a2cb47b2c00031
Ivanildo Pereira <suporte@scieve.com>
2022-12-02 11:40:47 -0300

    Listas e laços de repetição - Melhorando o código.

--- carro.js
+++ carro.js
@@ -5,26 +5,25 @@ let yCarros = [40, 96, 150];
 let velocidadeCarros = [2, 2.5, 3.2];
 
 function mostraCarro() {
-    for (let i = 0; i < imagemCarros.length; i += 1) {
+    for (let i = 0; i < imagemCarros.length; i = i + 1) {
         image(imagemCarros[i], xCarros[i], yCarros[i], 50, 40);
-
     }
 }
 
 function movimentaCarro() {
-    xCarros[0] -= velocidadeCarros[0];
-    xCarros[1] -= velocidadeCarros[1];
-    xCarros[2] -= velocidadeCarros[2];
+    for (let i = 0; i < imagemCarros.length; i = i + 1) {
+        xCarros[i] -= velocidadeCarros[i];
+    }
 }
 
 function voltaPosicaoInicialDoCarro() {
-    if (xCarros[0] < -50) {
-        xCarros[0] = 600
+    for (let i = 0; i < imagemCarros.length; i = i + 1) {
+        if (passouTodaATela(xCarros[i])) {
+            xCarros[i] = 600;
+        }
     }
-    if (xCarros[1] < -50) {
-        xCarros[1] = 600
-    }
-    if (xCarros[2] < -50) {
-        xCarros[2] = 600
-    }
+}
+
+function passouTodaATela(xCarro) {
+    return xCarro < - 50;
 }
