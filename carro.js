5164d0c9701d73042cb00dc6f19488a4f613e093
Ivanildo Pereira <suporte@scieve.com>
2022-12-02 10:39:57 -0300

    Dividindo o códio e carros para sempre - Dividindo código em arquivos.

---
+++ carro.js
@@ -0,0 +1,11 @@
+//carro
+let xCarro = 600;
+let yCarro = 40;
+
+function mostraCarro() {
+    image(imagemCarro, xCarro, yCarro, 50, 40);
+}
+
+function movimentaCarro() {
+    xCarro -= 2;
+}
---
+++ imagens.js
@@ -0,0 +1,9 @@
+let imagemDaEstrada;
+let imagemDoAtor;
+let imagemCarro;
+
+function preload() {
+    imagemDaEstrada = loadImage("imagens/estrada.png");
+    imagemDoAtor = loadImage("imagens/ator-1.png");
+    imagemCarro = loadImage("imagens/carro-1.png");
+  }
