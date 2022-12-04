8cd4068a51905ead4c7bee17b53a9563400ba798
Ivanildo Pereira <suporte@scieve.com>
2022-12-04 15:39:46 -0300

    Colisão, +carros e pontos.

--- ator.js
+++ ator.js
@@ -1,7 +1,8 @@
 //ator
-let xAtor = 100;
+let xAtor = 85;
 let yAtor = 366;
 let colisao = false;
+let meusPontos = 0;
 
 function mostraAtor() {
     image(imagemDoAtor, 100, yAtor, 30, 30);
@@ -21,11 +22,25 @@ function verificaColisao(){
     for (let i = 0; i < imagemCarros.length; i++){
       colisao = collideRectCircle(xCarros[i], yCarros[i], comprimentoCarro, alturaCarro, xAtor, yAtor, 15)
       if (colisao){
-        colidiu();
+        voltaAtorParaPosicaoInicial();
       }
     }
   }
 
-  function colidiu(){
+  function voltaAtorParaPosicaoInicial(){
     yAtor = 366;
   }
+
+  function incluiPontos(){
+    textAlign(CENTER);
+    textSize(25);
+    fill(color(255, 240, 60))
+    text(meusPontos, width / 5, 27);
+  }
+  
+  function marcaPonto(){
+    if (yAtor < 15){
+      meusPontos += 1;
+      voltaAtorParaPosicaoInicial();
+    }
+  } 
